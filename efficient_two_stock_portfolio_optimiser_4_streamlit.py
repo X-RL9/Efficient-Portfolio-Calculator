579e1b5f1df4dc449f0d67e35b003896fd0339f5
Robert Luke <165323930+X-RL9@users.noreply.github.com>
2026-06-20 23:00:20 +0100

    Update efficient_two_stock_portfolio_optimiser_4_streamlit.py

--- efficient_two_stock_portfolio_optimiser_4_streamlit.py
+++ efficient_two_stock_portfolio_optimiser_4_streamlit.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1x5jfh_4GUSpZZcJdwjd9fed5nf0zHwg_
 """
 
-!pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import numpy as np
